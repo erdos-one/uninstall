abe5c227d78d9aa9b38802c86f25013b4fd7985d
William W. Marx <m94@marx.sh>
2023-01-20 18:38:40 -0600

    Fix async error

--- src/index.ts
+++ src/index.ts
@@ -33,25 +33,41 @@ export default {
 
     // Get the packages registry
     const registryURL: string = "https://raw.githubusercontent.com/erdos-one/install/main/registry.json"
-    let packages: Packages = {}
-    fetch(registryURL).then(response => {
-      packages = response.json()
-    })
+    const resp = await fetch(registryURL)
+		const packages: Packages = await resp.json()
 
-    // Handle different path cases
-    if (path === "ls") {
-      // List package names if the path is "ls"
-      let ls: string = ""
-      Object.keys(packages).forEach(packageName => {
-        ls += `${packageName} [${packages[packageName].shell} → ${packages[packageName].homepage}]\n`
-      })
-      return new Response(ls)
-    } else if (path in packages) {
-      // Fetch the install script from and return it
-      return await fetch(packages[path].script)
-    } else {
-      // Return the help message to enable proper usage
-      return new Response(helpMessage);
-    }
-  }
+		// Handle different path cases
+		if (path === "ls") {
+			// List package names if the path is "ls"
+			let ls: string = ""
+
+			// Keep track of the longest name and shell to align the output
+			let longestName: number = 0
+			let longestShell: number = 0
+			Object.keys(packages).forEach(name => {
+				if (name.length > longestName) {
+					longestName = name.length
+				}
+				if (packages[name].shell.length > longestShell) {
+					longestShell = packages[name].shell.length
+				}
+			})
+
+			// Generate the output
+			Object.keys(packages).forEach(name => {
+				const space1: string = "  " + " ".repeat(longestName - name.length)
+				const space2: string = "  " + " ".repeat(longestShell - packages[name].shell.length)
+				ls += `${name}${space1}[${packages[name].shell}]${space2}${packages[name].homepage}\n`
+			})
+
+			return new Response(ls)
+		} else if (path in packages) {
+			// Fetch the install script from and return it
+			let script = await fetch(packages[path].script)
+			return script
+		} else {
+			// Return the help message to enable proper usage
+			return new Response(helpMessage)
+		}
+	}
 }
